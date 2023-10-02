921e2845c4333fb780c246a9e846d6a190345531
raymondhariyono <145003115+raymondhariyono@users.noreply.github.com>
2023-10-02 20:31:22 +0800

    Delete MODUL-0/MODUL-1 directory

--- MODUL-0/MODUL-1/python/PRAK0108-231081721007-RaymondHariyono.py
+++
@@ -1,9 +0,0 @@
-print("diketahui: ")
-putaran = int(input("Pak Dengklek mengelilingi taman = "))
-jarak = int(input("Jarak tempuh Pak Dengklek = "))
-
-keliling = jarak / putaran
-jariJari = keliling / (2 * 3.14)
-
-print("jawaban :")
-print(f"Jari-jari taman yang dikelilingi Pak Dengklek adalah {jariJari:.2f} kilometer")
--- MODUL-0/MODUL-1/python/PRAK0109-231081721007-RaymondHariyono.py
+++
@@ -1,6 +0,0 @@
-pasukanYuzhong = int(input ("Jumlah pasukan yang dibawa Yu Zhong = "))
-jumlahPahlawan = int (input ("Jumlah pahlawan = ") )
-
-pasukanYangHarusDikalahkan = pasukanYuzhong / jumlahPahlawan
-
-print(f"pasukan yang harus dikalahkan setiap pahlawan adalah {pasukanYangHarusDikalahkan:.0f}")
