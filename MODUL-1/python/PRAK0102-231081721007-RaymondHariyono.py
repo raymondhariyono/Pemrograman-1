7b551c7ed360c45291826588988abb77524ff820
raymondhariyono <145003115+raymondhariyono@users.noreply.github.com>
2023-10-02 16:22:37 +0800

    Delete MODUL-1 directory

--- MODUL-1/python/PRAK0102-231081721007-RaymondHariyono.py
+++
@@ -1,6 +0,0 @@
-a = float(input("Variabel a bernilai: "))
-b = float(input("Variabel b bernilai: "))
-c = float(input("Variabel c bernilai: "))
-
-hasil = a * b / c
-print(f"hasil dari a dikali b dibagi c adalah , {hasil:.6f}")
--- MODUL-1/python/PRAK0103-231081721007-RaymondHariyono.py
+++
@@ -1,7 +0,0 @@
-a = float(input("Variabel a bernilai "))
-b = float(input("Variabel b bernilai "))
-x = float(input("Variabel x bernilai "))
-y = float(input("Variabel y bernilai "))
-
-hasil = (a + b) * x / y
-print(f"hasil dari a ditambah b dikali x dan dibagi y adalah {hasil:.2f}")
